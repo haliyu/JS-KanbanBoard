4d333287d0a12e741756175f458dccfc52cff900
haliyu <61690728+haliyu@users.noreply.github.com>
2021-11-11 20:41:04 +0100

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -47,6 +47,10 @@ const create_item = () => {
 
 document.querySelectorAll('.drop').forEach(element => {
   element.addEventListener('drop', (event)=>{
-    event.preventDefault()
-    let id = event.dataTransfer.getData('text')})
+    event.preventDefault();
+    let id = event.dataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(id));
+    }
+    )
+  element.addEventListener('dragover', (event)=> event.preventDefault());
 });
