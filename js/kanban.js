bcd7d9f296d30b0d20187784dc2bbc618e9c0a2c
haliyu <61690728+haliyu@users.noreply.github.com>
2021-11-11 19:31:39 +0100

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -16,6 +16,24 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  let item = document.createElement('div');
+  item.classList.add(item);
+  item.id = 'item-' + order;
+  item.draggable = true;
+
+  item.addEventListener('dragstart', (e)=>{
+    return(
+      e.DataTransfer.setData('text', e.target.id)
+    );
+  });
+  item.addEventListener('dragend', (e)=>{
+    return(
+      e.DataTransfer.clearData()
+    )
+  });
+  let input = document.createElement('input');
+  item.appendChild(input);
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
