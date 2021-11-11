82e0d95f60070b2d52ed38bad69d1db7f8e9fd4e
haliyu <61690728+haliyu@users.noreply.github.com>
2021-11-11 19:56:02 +0100

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -29,7 +29,14 @@ const create_item = () => {
 
   let save_btn = document.createElement('button');
   save_btn.innerHTML='Save';
-  save_btn.addEventListener('click', ()=>{} )
+  save_btn.addEventListener('click', ()=>{ 
+    error.innerHTML= '';
+    if (input.value !== '') {
+      
+    }
+  } );
+
+
 
 };
 
